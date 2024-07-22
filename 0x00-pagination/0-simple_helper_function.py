dd1795de101db6ef4fdd14344a47ece218ebbb82
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-22 11:22:58 +0300

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,3 +2,13 @@
 """
 Simpler Helper
 """
+
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """index_range"""
+    end_index = page * page_size
+    start_index = end_index - page_size
+    return (start_index, end_index)
