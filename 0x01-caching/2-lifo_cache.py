90ab26c42d2089becb06519b14308809a167feaf
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-24 12:35:54 +0300

    lifo refactor

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -16,7 +16,7 @@ class LIFOCache(BaseCaching):
         if key is not None and item is not None:
             if len(self.cache_data) >= self.MAX_ITEMS:
                 if self.cache_data:
-                    key_to_discard = list(self.cache_data.keys())[-1]
+                    key_to_discard = next(reversed(self.cache_data))
                     self.cache_data.pop(key_to_discard)
                     print("DISCARD: {}".format(key_to_discard))
             self.cache_data[key] = item
