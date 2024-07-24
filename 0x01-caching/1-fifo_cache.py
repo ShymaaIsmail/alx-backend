ebdaacfd117d06a3a8f5bc453d0efb0cc4c60b7e
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-24 12:21:48 +0300

    kkk

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,51 +1,32 @@
-#!/usr/bin/python3
-"""FIFO Cache Replacement Implementation Class
-"""
-from threading import RLock
+#!/usr/bin/env python3
+"""fifo cache system"""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """
-    An implementation of FIFO(First In Fisrt Out) Cache
+    """FIFO Caching"""
 
-    Attributes:
-        __keys (list): Stores cache keys in order of entry using `.append`
-        __rlock (RLock): Lock accessed resources to prevent race condition
-    """
     def __init__(self):
-        """ Instantiation method, sets instance attributes
-        """
+        """initialize FICFO Class"""
         super().__init__()
-        self.__keys = []
-        self.__rlock = RLock()
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
+        """put value of a key"""
         if key is not None and item is not None:
-            keyOut = self._balance(key)
-            with self.__rlock:
-                self.cache_data.update({key: item})
-            if keyOut is not None:
-                print('DISCARD: {}'.format(keyOut))
+            if len(self.cache_data.items()) >= self.MAX_ITEMS:
+                key_to_discard = (k := next(iter(self.cache_data)), self.cache_data.pop(k))
+                print("DISCARD: {}".format(key_to_discard[0]))
+                self.cache_data[key] = item
+            else:
+                self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
-        """
-        with self.__rlock:
-            return self.cache_data.get(key, None)
-
-    def _balance(self, keyIn):
-        """ Removes the oldest item from the cache at MAX size
-        """
-        keyOut = None
-        with self.__rlock:
-            if keyIn not in self.__keys:
-                keysLength = len(self.__keys)
-                if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-                    keyOut = self.__keys.pop(0)
-                    self.cache_data.pop(keyOut)
-                self.__keys.insert(keysLength, keyIn)
-        return keyOut
+        """get a value of a key"""
+        value = None
+        try:
+            if key:
+                value = self.cache_data[key]
+        except KeyError:
+            return value
+        return value
