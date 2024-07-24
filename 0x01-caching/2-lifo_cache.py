b3a1fd1c7ce672bd1a88b45c6bb27a1cd400b8cb
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-24 12:34:40 +0300

    task 2 lifo

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,33 +1,26 @@
 #!/usr/bin/env python3
-"""fifo cache"""
+"""LIFO Cache System"""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    """FIFO Caching"""
+class LIFOCache(BaseCaching):
+    """LIFO Caching"""
 
     def __init__(self):
-        """init class"""
+        """Initialize LIFOCache class"""
         super().__init__()
 
     def put(self, key, item):
-        """put value of a key"""
+        """Put value of a key"""
         if key is not None and item is not None:
-            if len(self.cache_data.items()) >= self.MAX_ITEMS:
-                key_to_discard = (k := next(iter(self.cache_data)),
-                                  self.cache_data.pop(k))
-                print("DISCARD: {}".format(key_to_discard[0]))
-                self.cache_data[key] = item
-            else:
-                self.cache_data[key] = item
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                if self.cache_data:
+                    key_to_discard = list(self.cache_data.keys())[-1]
+                    self.cache_data.pop(key_to_discard)
+                    print("DISCARD: {}".format(key_to_discard))
+            self.cache_data[key] = item
 
     def get(self, key):
-        """get a value of a key"""
-        value = None
-        try:
-            if key:
-                value = self.cache_data[key]
-        except KeyError:
-            return value
-        return value
+        """Get a value of a key"""
+        return self.cache_data.get(key, None)
