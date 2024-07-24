5f1df94e9fe982361686e1011c4481dbe016654c
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-24 11:08:43 +0300

    task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
 """ basic cache"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic Caching"""
+
+    def __init__(self):
+        """init"""
+        super().__init__()
+
+    def put(self, key, item):
+        """put"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get"""
+        value = None
+        try:
+            if key:
+                value = self.cache_data[key]
+        except KeyError:
+            return value
+        return value
