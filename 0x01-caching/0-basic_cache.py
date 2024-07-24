acc3fd8537ebbef07bd4f5e82f91ee4e563fd20c
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-24 11:39:43 +0300

    basic

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,12 +7,17 @@ BaseCaching = __import__('base_caching').BaseCaching
 class BasicCache(BaseCaching):
     """Basic Caching"""
 
-
     def put(self, key, item):
         """put"""
-        if key and item:
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
         """get"""
-        return self.cache_data.get(key, None)
+        value = None
+        try:
+            if key:
+                value = self.cache_data[key]
+        except KeyError:
+            return value
+        return value
