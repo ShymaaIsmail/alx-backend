113677f9b92ab75f65e2b2f2b78ed702c56d32be
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-24 12:09:42 +0300

    task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,2 +1,33 @@
-#!/usr/bin/python3
-""" fifo cache """
+#!/usr/bin/env python3
+"""fifo cache"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFO Caching"""
+
+    def __init__(self):
+        """init class"""
+        super().__init__()
+
+    def put(self, key, item):
+        """put value of a key"""
+        if key is not None and item is not None:
+            if len(self.cache_data.items()) >= self.MAX_ITEMS:
+                key_to_discard = (k := next(iter(self.cache_data)),
+                                  self.cache_data.pop(k))
+                print("DISCARD: {}".format(key_to_discard[0]))
+                self.cache_data[key] = item
+            else:
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """get a value of a key"""
+        value = None
+        try:
+            if key:
+                value = self.cache_data[key]
+        except KeyError:
+            return value
+        return value
