9a077e2f37ca83278336860b8917986e81ca2a6c
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-28 11:02:55 +0300

    task 4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,33 +1,39 @@
 #!/usr/bin/python3
-""" mru cache """
-from collections import OrderedDict
+""" MRU Cache """
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """mruCache class"""
+    """MRUCache class"""
     def __init__(self):
-        """initialize mruCache"""
+        """Initialize MRUCache"""
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.order = []
 
     def put(self, key, item):
-        """put item in cache"""
+        """Put item in cache"""
         if key is None or item is None:
             return
+
         if key in self.cache_data:
-            # Move the existing key to the end to show it was recently used
-            self.cache_data.move_to_end(key, last= False)
+            # Remove the key from the current position in order list
+            self.order.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # Discard the most recently used item
+            mru_key = self.order.pop()
+            del self.cache_data[mru_key]
+            print(f"DISCARD: {mru_key}")
+
+        # Add the key to the cache and update the order
         self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            # Pop the first item (most recently used)
-            discarded_key, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {discarded_key}")
+        self.order.append(key)
 
     def get(self, key):
-        """get item in cache"""
+        """Get item from cache"""
         if key is None or key not in self.cache_data:
             return None
-        # Move the accessed key to the beginning to show it was recently used
-        self.cache_data.move_to_end(key, last= False)
+
+        # Move the accessed key to the end to mark it as most recently used
+        self.order.remove(key)
+        self.order.append(key)
         return self.cache_data[key]
