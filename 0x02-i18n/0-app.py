bb9ccbaa1e197ed62c7254d4e6c39bc16afbeee2
shymaaismail <shymaa.m.ismail@gmail.com>
2024-08-01 11:50:09 +0300

    fix 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,10 +7,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     """Task 0"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
