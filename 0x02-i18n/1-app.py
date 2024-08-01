10cbad139ed1fd6cd7532858288a00f2a9bad262
shymaaismail <shymaa.m.ismail@gmail.com>
2024-08-01 12:09:20 +0300

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -13,8 +13,15 @@ babel = Babel(app)
 class Config():
     """Config class"""
     LANGUAGES = ["en", "fr"]
+
 app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+
+@app.route("/")
+def index():
+    """Task 0"""
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
