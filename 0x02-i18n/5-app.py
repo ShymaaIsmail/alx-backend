c77b74e40ae67b21d81a6299d83c6c4019d76282
shymaaismail <shymaa.m.ismail@gmail.com>
2024-08-04 12:06:53 +0300

    task5

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, render_template, g
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+
+
+class Config():
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """get user"""
+    login_as = request.args.get('login_as')
+    if login_as:
+        user_id = int(login_as)
+        return users.get(user_id)
+    return None
+
+
+@app.before_request
+def before_request():
+    """before request"""
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale():
+    """get locale """
+    if g.user and g.user['locale'] in app.config['LANGUAGES']:
+        return g.user['locale']
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Task 5"""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
